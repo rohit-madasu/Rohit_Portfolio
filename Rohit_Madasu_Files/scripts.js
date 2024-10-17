b39caff268a35a4dc9dabcf63ac966e8fe31e7f8
rohit-madasu <rohitmadasu@gmail.com>
2024-10-17 21:18:59 +0530

    adding to test the js file

---
+++ Rohit_Madasu_Files/scripts.js
@@ -0,0 +1,10 @@
+// Display a welcome alert when the page loads
+window.onload = function() {
+    alert("Welcome to Rohit Madasu's Portfolio!");
+};
+
+// Function to show contact details on button click
+function showContact() {
+    const contactInfo = document.getElementById("contact-info");
+    contactInfo.style.display = "block";
+}
